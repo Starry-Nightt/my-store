e530599cf72e410249c821919deb4aa79d82fb71
dangminhtien051202@gmail.com <dangminhtien051202@gmail.com>
2023-09-11 00:27:18 +0700

    fix bug

--- src/app/layouts/components/header/header.component.ts
+++ src/app/layouts/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { User } from '@models/user.model';
 import { BehaviorSubject } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,7 +14,11 @@ import { ProductService } from 'src/app/services/product.service';
 export class HeaderComponent implements OnInit {
   user$ = new BehaviorSubject<User>(undefined);
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     this.user$ = this.authService.user$;
@@ -24,6 +29,10 @@ export class HeaderComponent implements OnInit {
   }
 
   logout() {
-    this.authService.logout().subscribe();
+    this.authService.logout().subscribe(() => {
+      this.cartService.cartDto$.next([]);
+      this.cartService.cartInfo.next(undefined);
+      this.router.navigate(['/product']);
+    });
   }
 }
